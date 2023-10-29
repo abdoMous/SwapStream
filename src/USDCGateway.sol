aaacfe0103189411337dd06823fc92b5a96d7e53
Abderrahmane Moussaoui <msiabdrahmn@gmail.com>
2023-10-29 17:17:42 +0100

    functions needed

---
+++ src/USDCGateway.sol
@@ -0,0 +1,112 @@
+// SPDX-License-Identifier: MIT
+
+// Layout of Contract:
+// version
+// imports
+// interfaces, libraries, contracts
+// errors
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+pragma solidity 0.8.22;
+
+/*
+ * @title USDCGateway
+ * @dev This contract allows users to deposit any token into the smart contract and receive USDC in return.
+ * The contract owner can withdraw USDC from the smart contract.
+ * The contract uses the Cow Protocol to handle the swap and ensure that the contract only receives USDC.
+ * The contract owner can update the address of the Cow Protocol if needed.
+ */
+contract USDCGateway {
+    /////////////////////
+    // State Variables //
+    /////////////////////
+
+    // Address of the Cow Protocol
+    address public s_cowProtocolAddress;
+
+    // Mapping to store USDC balance for each address
+    mapping(address => uint256) public s_usdcBalances;
+
+    ////////////
+    // Events //
+    ////////////
+    event Deposited(address indexed user, uint256 amount, address token);
+    event ConvertedToUSDC(address indexed user, uint256 amount);
+    event Withdrawn(address indexed user, uint256 amount);
+
+    ///////////////
+    // Functions //
+    ///////////////
+
+    /////////////////////////
+    // External Functions //
+    ////////////////////////
+
+    constructor(address _cowProtocolAddress) {
+        s_cowProtocolAddress = _cowProtocolAddress;
+    }
+
+    // Allows users to deposit any token into the smart contract
+    function depositAssets() external {
+        // TODO: Logic to deposit assets
+        emit Deposited(msg.sender, 0, address(0)); // Update the amount and token address accordingly
+    }
+
+    // Allows users or the contract owner to withdraw USDC from the smart contract
+    function withdrawUSDC() external {
+        // TODO: Logic to withdraw USDC
+        emit Withdrawn(msg.sender, 0);
+    }
+
+    // If the address of the Cow Protocol might change, the contract owner can update it
+    function setCowProtocolAddress(address _newAddress) external {
+        // TODO: Logic to set the new Cow Protocol address
+        s_cowProtocolAddress = _newAddress;
+    }
+
+    ///////////////////////////////////////
+    // Private & Internal veiw Functions //
+    ///////////////////////////////////////
+
+    // Interacts with the Cow Protocol to handle the swap and ensure that the contract only receives USDC
+    function convertToUSDC() internal {
+        // TODO: Logic to convert assets to USDC using Cow Protocol
+        emit ConvertedToUSDC(msg.sender, 0); // Update the amount accordingly
+    }
+
+    //////////////////////////////////////
+    // Public & External View Functions //
+    //////////////////////////////////////
+
+    // Fetches the current conversion rate from the Cow Protocol for a specific token to USDC
+    function getConversionRate(address token) public view returns (uint256) {
+        // TODO: Logic to fetch conversion rate from Cow Protocol
+        return 0; // Placeholder value
+    }
+
+    // Returns the total amount of USDC currently in the smart contract
+    function totalUSDCBalance() public view returns (uint256) {
+        // TODO: Logic to return total USDC balance
+        return 0; // Placeholder value
+    }
+
+    // Provides a list of pending deposits that are waiting to be converted
+    function queryPendingDeposits() public view returns (address[] memory) {
+        // TODO: Logic to return list of pending deposits
+        return new address[](0); // Placeholder value
+    }
+}
